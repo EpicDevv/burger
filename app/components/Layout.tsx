bc207ee6dfe876c55ea524d0602f7225d2704695
EpicDevv <zayne.mngmt@gmail.com>
2024-01-27 03:05:17 -0800

    add footer

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,10 +1,11 @@
 import { Header } from "./Header";
-
+import { Footer } from "./Footer";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Header />
       {children}
+      <Footer />
     </div>
   );
 };
