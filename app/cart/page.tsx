37483638048f479b30d0382318606b358b7b1aac
EpicDevv <zayne.mngmt@gmail.com>
2024-01-29 06:14:01 -0800

    finishing touches

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -26,7 +26,7 @@ export default function Cart() {
     e.preventDefault();
   };
   return (
-    <div className="bg-white dark:bg-[#0F0F0F]">
+    <div className="bg-white min-h-screen dark:bg-[#0F0F0F]">
       <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8">
         <h1 className="text-3xl font-bold tracking-tight dark:text-[#008170] text-gray-900 sm:text-4xl">
           Shopping Cart
